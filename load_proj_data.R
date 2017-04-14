936acbb5789f4255228d71fd96632cde32a1cb7d
cdo03c <cdo03c@gmail.com>
2017-04-13 21:23:27 -0400

    Fixed Data Structure
    Added the ability to download the detain the script, fixed the headers
    to read in correctly for all the variables in the NHES study, and
    figured out that only the first 356 variables are actual variables.

--- load_proj_data.R
+++ load_proj_data.R
@@ -7,40 +7,48 @@ library(pdftools)
 
 ###Load the NHES 2012 code book for extracting the data
 
-#Use PDF_Tools to create a character vector from NHES 2012 codebook
+#Download the .pdf of the NHES 2012 code book if it does not exist in the
+#working directory and use PDF_Tools to create a character vector from NHES 2012
+#codebook
+if(!file.exists("./NHES_2012_pfi_codebook.pdf")){
+  download.file(url = "https://raw.githubusercontent.com/cdo03c/Stat504Proj/master/NHES_2012_pfi_codebook.pdf",
+                destfile = "./NHES_2012_pfi_codebook.pdf")
+}
 dict = pdf_text("./NHES_2012_pfi_codebook.pdf")
 
 #Loop through the character vector and extract all the variable names into a
 #new character vector called header and extract the fixed width values into a 
 #vector called widths
 header = vector()
-widths = vector()
 for(i in 1:length(dict)){
-  m <- gregexpr('([A-Z]{5,}\\d+|[A-Z]_[A-Z]{5,}|[A-Z]{4,})', dict[i])
+  m <- gregexpr('([A-Z]{4,}\\d+|[A-Z]{1,}\\d+[A-Z]{2,}|[A-Z]_[A-Z]{5,}|[A-Z]{4,})', dict[i])
   vars = unlist(regmatches(dict[i], m))
   if(!identical(vars, character(0))){
-    vars = vars[!vars %in% c('NHES','IMPUTATION', 'FINAL', 'REPLICATE', 'WEIGHT')]
+    vars = vars[!vars %in% c('NHES','IMPUTATION', 'FINAL', 'REPLICATE',
+                             'ADHD', 'TANF', 'CHIP', 'TAYLOR','SERIES',
+                             'STRATUM','WEIGHT')]
     header = c(header,unique(vars))
   }
-  w <- gregexpr('(\\d+-\\d+)', dict[i])
-  ws = unlist(regmatches(dict[i], w))
-  ws = strsplit(ws, '-')
-  for(y in 1:length(ws)){
-    width = as.integer(unlist(ws[y]))[2] - as.integer(unlist(ws[y]))[1]+1
-    if(width < 14){
-      widths = c(widths,width)
-    }
-  }
 }
-#print(header)
-#print(widths)
+
+#Trims the header and width to just the variables form the NHES survey and
+#removes all the weight and imputation variables.
+header = header[1:356]
+widths = c(12,8,1,rep(2,353))
 
 #Load data table for NHES 2012 study
+if(!file.exists("./pfi_pu_pert_ascii.dat")){
+  download.file(url = "https://raw.githubusercontent.com/cdo03c/Stat504Proj/master/pfi_pu_pert_ascii.dat",
+                destfile = "./NHES_2012_pfi_codebook.pdf")
+}
 df = read.fwf("./pfi_pu_pert_ascii.dat", widths = widths)
 head(df)
 length(header)
 
-#Subset to the first 91 columns of data
-df2 = df[1:91]
-colnames(df2) = header[1:91]
+#Subset to the first 356 columns of data to match the header character vector
+df2 = df[1:356]
+colnames(df2) = header
 head(df2,1)
+
+#Subset data by PATH variable where E stands for elemntary student
+df2 = df2[df2$PATH == 'E',]
